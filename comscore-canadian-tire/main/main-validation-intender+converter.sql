4b84f1169f7ad3123ce237faa547b4bb6f0797e9
PY <51064667+pyxn@users.noreply.github.com>
2023-04-05 12:00:07 -0400

    only include users who visited a competitor site

--- comscore-canadian-tire/main/main-validation-intender+converter.sql
+++ comscore-canadian-tire/main/main-validation-intender+converter.sql
@@ -88,6 +88,27 @@ AND
         )
     )
 )
+-- ********************************************************************
+-- ONLY INCLUDE USERS WHO HAVE VISITED A COMPETITOR SITE
+-- ********************************************************************
+AND 
+(
+    (domain LIKE '%canadiantire.ca%'     OR event_detail LIKE '%canadiantire.ca%')     OR
+    (domain LIKE '%walmart.ca%'          OR event_detail LIKE '%walmart.ca%')          OR
+    (domain LIKE '%amazon%'              OR event_detail LIKE '%amazon.ca%')           OR
+    (domain LIKE '%amzn%'                OR event_detail LIKE '%amazon.ca%')           OR
+    (domain LIKE '%costco.ca%'           OR event_detail LIKE '%costco.ca%')           OR
+    (domain LIKE '%sobeys.com%'          OR event_detail LIKE '%sobeys.com%')          OR
+    (domain LIKE '%petland.ca%'          OR event_detail LIKE '%petland.ca%')          OR
+    (domain LIKE '%petvalu.ca%'          OR event_detail LIKE '%petvalu.ca%')          OR
+    (domain LIKE '%petsmart.ca%'         OR event_detail LIKE '%petsmart.ca%')         OR
+    (domain LIKE '%baileyblu.com%'       OR event_detail LIKE '%baileyblu.com%')       OR
+    (domain LIKE '%chico.ca%'            OR event_detail LIKE '%chico.ca%')            OR
+    (domain LIKE '%mondou.com%'          OR event_detail LIKE '%mondou.com%')          OR
+    (domain LIKE '%pattesgriffes.com%'   OR event_detail LIKE '%pattesgriffes.com%')   OR
+    (domain LIKE '%tailblazerspets.com%' OR event_detail LIKE '%tailblazerspets.com%') OR
+    (domain LIKE '%wbu.com%'             OR event_detail LIKE '%wbu.com%')                         
+) 
 
 -- ********************************************************************
 -- DOMAIN CONVERTER GROUP (COMMENT OUT IF NOT NEEDED)
