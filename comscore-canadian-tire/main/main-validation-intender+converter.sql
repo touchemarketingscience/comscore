aa4b8ceb9d8ccfb18d0c5144304f9909632b4b5a
PY <51064667+pyxn@users.noreply.github.com>
2023-04-05 11:52:53 -0400

    update converter identification

--- comscore-canadian-tire/main/main-validation-intender+converter.sql
+++ comscore-canadian-tire/main/main-validation-intender+converter.sql
@@ -39,54 +39,54 @@ AND
     -- ********************************************************************
     guid IN 
     (
-    SELECT DISTINCT guid FROM spectrum_comscore.clickstream_ca
-    WHERE 
-    (
-        (
-            (domain LIKE '%canadiantire.ca%'     OR event_detail LIKE '%canadiantire.ca%')     OR
-            (domain LIKE '%walmart.ca%'          OR event_detail LIKE '%walmart.ca%')          OR
-            (domain LIKE '%amazon%'              OR event_detail LIKE '%amazon.ca%')           OR
-            (domain LIKE '%amzn%'                OR event_detail LIKE '%amazon.ca%')           OR
-            (domain LIKE '%costco.ca%'           OR event_detail LIKE '%costco.ca%')           OR
-            (domain LIKE '%sobeys.com%'          OR event_detail LIKE '%sobeys.com%')          OR
-            (domain LIKE '%petland.ca%'          OR event_detail LIKE '%petland.ca%')          OR
-            (domain LIKE '%petvalu.ca%'          OR event_detail LIKE '%petvalu.ca%')          OR
-            (domain LIKE '%petsmart.ca%'         OR event_detail LIKE '%petsmart.ca%')         OR
-            (domain LIKE '%baileyblu.com%'       OR event_detail LIKE '%baileyblu.com%')       OR
-            (domain LIKE '%chico.ca%'            OR event_detail LIKE '%chico.ca%')            OR
-            (domain LIKE '%mondou.com%'          OR event_detail LIKE '%mondou.com%')          OR
-            (domain LIKE '%pattesgriffes.com%'   OR event_detail LIKE '%pattesgriffes.com%')   OR
-            (domain LIKE '%tailblazerspets.com%' OR event_detail LIKE '%tailblazerspets.com%') OR
-            (domain LIKE '%wbu.com%'             OR event_detail LIKE '%wbu.com%')                         
-        ) 
-        AND 
+        SELECT DISTINCT guid FROM spectrum_comscore.clickstream_ca
+        WHERE 
         (
             (
-                zvelo LIKE '%Pets%' OR zvelo_category LIKE '%Pets%' OR zvelo_subcategory LIKE '%Pets%'
+                (domain LIKE '%canadiantire.ca%'     OR event_detail LIKE '%canadiantire.ca%')     OR
+                (domain LIKE '%walmart.ca%'          OR event_detail LIKE '%walmart.ca%')          OR
+                (domain LIKE '%amazon%'              OR event_detail LIKE '%amazon.ca%')           OR
+                (domain LIKE '%amzn%'                OR event_detail LIKE '%amazon.ca%')           OR
+                (domain LIKE '%costco.ca%'           OR event_detail LIKE '%costco.ca%')           OR
+                (domain LIKE '%sobeys.com%'          OR event_detail LIKE '%sobeys.com%')          OR
+                (domain LIKE '%petland.ca%'          OR event_detail LIKE '%petland.ca%')          OR
+                (domain LIKE '%petvalu.ca%'          OR event_detail LIKE '%petvalu.ca%')          OR
+                (domain LIKE '%petsmart.ca%'         OR event_detail LIKE '%petsmart.ca%')         OR
+                (domain LIKE '%baileyblu.com%'       OR event_detail LIKE '%baileyblu.com%')       OR
+                (domain LIKE '%chico.ca%'            OR event_detail LIKE '%chico.ca%')            OR
+                (domain LIKE '%mondou.com%'          OR event_detail LIKE '%mondou.com%')          OR
+                (domain LIKE '%pattesgriffes.com%'   OR event_detail LIKE '%pattesgriffes.com%')   OR
+                (domain LIKE '%tailblazerspets.com%' OR event_detail LIKE '%tailblazerspets.com%') OR
+                (domain LIKE '%wbu.com%'             OR event_detail LIKE '%wbu.com%')                         
             ) 
-            OR
+            AND 
             (
-                domain NOT LIKE '%estjet%' 
-                AND 
                 (
-                event_detail LIKE '%animalerie%'    OR 
-                event_detail LIKE '%animaux%'       OR 
-                event_detail LIKE '%pets%'          OR 
-                event_detail LIKE '%pet-%'          OR 
-                event_detail LIKE '%pet/%'          OR 
-                event_detail LIKE '%pet\.%' 
+                    zvelo LIKE '%Pets%' OR zvelo_category LIKE '%Pets%' OR zvelo_subcategory LIKE '%Pets%'
                 ) 
-                AND 
-                ( 
-                event_detail NOT LIKE '%peti%'      OR
-                event_detail NOT LIKE '%peta%'      OR 
-                event_detail NOT LIKE '%petr%'      OR 
-                event_detail NOT LIKE '%ppet%'
+                OR
+                (
+                    domain NOT LIKE '%estjet%' 
+                    AND 
+                    (
+                    event_detail LIKE '%animalerie%'    OR 
+                    event_detail LIKE '%animaux%'       OR 
+                    event_detail LIKE '%pets%'          OR 
+                    event_detail LIKE '%pet-%'          OR 
+                    event_detail LIKE '%pet/%'          OR 
+                    event_detail LIKE '%pet\.%' 
+                    ) 
+                    AND 
+                    ( 
+                    event_detail NOT LIKE '%peti%'      OR
+                    event_detail NOT LIKE '%peta%'      OR 
+                    event_detail NOT LIKE '%petr%'      OR 
+                    event_detail NOT LIKE '%ppet%'
+                    )
                 )
             )
         )
     )
-    
 )
 
 -- ********************************************************************
@@ -108,7 +108,5 @@ AND
     (event_detail LIKE '%checkout%' AND event_detail LIKE '%confirm%')      OR
     (event_detail LIKE '%confirm%' AND event_detail LIKE '%checkout%')      
 )
-
-
 
 LIMIT 50000;
