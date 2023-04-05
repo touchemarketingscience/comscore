ac88241515d2d445c12d6527c0a0f961e466e44e
PY <51064667+pyxn@users.noreply.github.com>
2023-04-05 15:50:45 -0400

    test

--- comscore-canadian-tire/main/main-intenders.sql
+++ comscore-canadian-tire/main/main-intenders.sql
@@ -25,60 +25,160 @@ SELECT
 FROM
     spectrum_comscore.clickstream_ca
 WHERE
-    (
-        (
-            date_part(year, calendar_date) = 2021
-            OR date_part(year, calendar_date) = 2022
-        )
+    
+(
+    date_part(year, calendar_date) = 2021OR date_part(year, calendar_date) = 2022
+)
 
-        AND (
-                (domain LIKE '%petland.ca%')
-            OR (domain LIKE '%petvalu.ca%')
-            OR (domain LIKE '%petsmart.ca%')
-            OR (domain LIKE '%baileyblu.com%')
-            OR (domain LIKE '%chico.ca%')
-            OR (domain LIKE '%mondou.com%')
-            OR (domain LIKE '%pattesgriffes.com%')
-            OR (domain LIKE '%tailblazerspets.com%')
-            OR (domain LIKE '%wbu.com%')
-        )
-        AND (
+AND 
+(
+    (
+        (domain LIKE '%petland.ca%')
+    OR (domain LIKE '%petvalu.ca%')
+    OR (domain LIKE '%petsmart.ca%')
+    OR (domain LIKE '%baileyblu.com%')
+    OR (domain LIKE '%chico.ca%')
+    OR (domain LIKE '%mondou.com%')
+    OR (domain LIKE '%pattesgriffes.com%')
+    OR (domain LIKE '%tailblazerspets.com%')
+    OR (domain LIKE '%wbu.com%')
+    )
+
+    OR
+
+    (
+        -- *************************************************
+        -- CANADIAN TIRE
+        -- *************************************************
+        (
+            domain LIKE '%canadiantire.ca%'
+            AND 
             (
                 (
-                    domain LIKE '%canadiantire.ca%'
-                    AND event_detail LIKE '%pet-care%'
-                    OR domain LIKE '%canadiantire.ca%'
-                    AND event_detail LIKE '%animalerie%'
+                    event_detail LIKE '%animalerie%'
+                    OR event_detail LIKE '%pet-care%'
                 )
-                OR (domain LIKE '%walmart.ca%')
-                OR (
-                    domain LIKE '%amazon%'
-                    OR domain LIKE '%amzn%'
+                AND 
+                (
+                    event_detail NOT LIKE '%peti%'
+                    OR event_detail NOT LIKE '%peta%'
+                    OR event_detail NOT LIKE '%petr%'
+                    OR event_detail NOT LIKE '%ppet%'
                 )
-                OR (domain LIKE '%costco.ca%')
-                OR (
-                    domain LIKE '%sobeys.com%'
-                    AND event_detail LIKE '%pet%'
-                )
-            )
-            AND (
-                event_detail LIKE '%animalerie%'
-                OR event_detail LIKE '%animaux%'
-                OR event_detail LIKE '%pets%'
-                OR event_detail LIKE '%pet-%'
-                OR event_detail LIKE '%pet/%'
-                OR event_detail LIKE '%pet\.%'
-            )
-            AND (
-                event_detail NOT LIKE '%peti%'
-                OR event_detail NOT LIKE '%peta%'
-                OR event_detail NOT LIKE '%petr%'
-                OR event_detail NOT LIKE '%ppet%'
             )
         )
 
-        
+        OR
+        -- *************************************************
+        -- WALMART
+        -- *************************************************
+        (
+            domain LIKE '%walmart.ca%' AND 
+            (
+                (
+                    event_detail LIKE '%animalerie%'
+                    OR event_detail LIKE '%animaux%'
+                    OR event_detail LIKE '%pets%'
+                    OR event_detail LIKE '%pet-%'
+                    OR event_detail LIKE '%pet/%'
+                    OR event_detail LIKE '%pet\.%'
+                )
+                AND 
+                (
+                    event_detail NOT LIKE '%peti%'
+                    OR event_detail NOT LIKE '%peta%'
+                    OR event_detail NOT LIKE '%petr%'
+                    OR event_detail NOT LIKE '%ppet%'
+                )
+            )
+        )
+
+         OR
+        -- *************************************************
+        -- AMAZON
+        -- *************************************************
+        (
+            domain LIKE '%amazon%'
+            OR domain LIKE '%amzn%' AND 
+            (
+                (
+                    event_detail LIKE '%animalerie%'
+                    OR event_detail LIKE '%animaux%'
+                    OR event_detail LIKE '%pets%'
+                    OR event_detail LIKE '%pet-%'
+                    OR event_detail LIKE '%pet/%'
+                    OR event_detail LIKE '%pet\.%'
+                )
+                AND 
+                (
+                    event_detail NOT LIKE '%peti%'
+                    OR event_detail NOT LIKE '%peta%'
+                    OR event_detail NOT LIKE '%petr%'
+                    OR event_detail NOT LIKE '%ppet%'
+                )
+            )
+        )
+
+        OR
+        -- *************************************************
+        -- AMAZON
+        -- *************************************************
+        (
+            (domain LIKE '%costco.ca%') AND
+            (
+                (
+                    event_detail LIKE '%animalerie%'
+                    OR event_detail LIKE '%animaux%'
+                    OR event_detail LIKE '%pets%'
+                    OR event_detail LIKE '%pet-%'
+                    OR event_detail LIKE '%pet/%'
+                    OR event_detail LIKE '%pet\.%'
+                )
+                AND 
+                (
+                    event_detail NOT LIKE '%peti%'
+                    OR event_detail NOT LIKE '%peta%'
+                    OR event_detail NOT LIKE '%petr%'
+                    OR event_detail NOT LIKE '%ppet%'
+                )
+            )
+        )
+         OR
+        -- *************************************************
+        -- SOBEYS
+        -- *************************************************
+        (
+            domain LIKE '%sobeys.com%' AND
+            (
+                (
+                    event_detail LIKE '%animalerie%'
+                    OR event_detail LIKE '%pet%'
+                )
+                AND 
+                (
+                    event_detail NOT LIKE '%peti%'
+                    OR event_detail NOT LIKE '%peta%'
+                    OR event_detail NOT LIKE '%petr%'
+                    OR event_detail NOT LIKE '%ppet%'
+                )
+            )
+        )
+
+
+
+
     )
+)
+
+    
+GROUP BY
+    1
+ORDER BY
+    2 desc
+LIMIT
+    10000;
+
+
     /*
      -- *** CONVERTER *** --
      AND
@@ -97,9 +197,3 @@ WHERE
      (event_detail LIKE '%order%' AND event_detail LIKE '%account%')         
      )
      */
-GROUP BY
-    1
-ORDER BY
-    2 desc
-LIMIT
-    10000;
