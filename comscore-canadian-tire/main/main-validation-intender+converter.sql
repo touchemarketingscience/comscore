415648f14bc74501a2dcec96d0beb89f19d27b17
PY <51064667+pyxn@users.noreply.github.com>
2023-04-05 12:06:59 -0400

    update converter urls to competitors only

--- comscore-canadian-tire/main/main-validation-intender+converter.sql
+++ comscore-canadian-tire/main/main-validation-intender+converter.sql
@@ -12,9 +12,8 @@ WHERE
 ) 
 AND 
 (   
-    
     -- ********************************************************************
-    -- ONLY INCLUDE USERS WHO HAVE VISITED A COMPETITOR SITE
+    -- ONLY INCLUDE USERS WHO HAVE VISITED A COMPETITOR SITE (INTENDER GROUP)
     -- ********************************************************************
     guid IN 
     (
@@ -73,19 +72,39 @@ AND
 -- ********************************************************************
 AND 
 (   
-    (event_detail LIKE '%shopping-cart%')                                   OR
-    (event_detail LIKE '%/cart%')                                           OR
-    (event_detail LIKE '%checkout%')                                        OR
-    (event_detail LIKE '%shop%' AND event_detail LIKE '%cart%')             OR
-    (event_detail LIKE '%cart%' AND event_detail LIKE '%shop%')             OR
-    (event_detail LIKE '%order%' AND event_detail LIKE '%history%')         OR
-    (event_detail LIKE '%cart%' AND event_detail LIKE '%checkout%')         OR
-    (event_detail LIKE '%checkout%' AND event_detail LIKE '%cart%')         OR
-    (event_detail LIKE '%recent%' AND event_detail LIKE '%order%')          OR
-    (event_detail LIKE '%order%' AND event_detail LIKE '%recent%')          OR
-    (event_detail LIKE '%account%' AND event_detail LIKE '%order%')         OR
-    (event_detail LIKE '%checkout%' AND event_detail LIKE '%confirm%')      OR
-    (event_detail LIKE '%confirm%' AND event_detail LIKE '%checkout%')      
+    (
+        (domain LIKE '%canadiantire.ca%'     OR event_detail LIKE '%canadiantire.ca%')     OR
+        (domain LIKE '%walmart.ca%'          OR event_detail LIKE '%walmart.ca%')          OR
+        (domain LIKE '%amazon%'              OR event_detail LIKE '%amazon.ca%')           OR
+        (domain LIKE '%amzn%'                OR event_detail LIKE '%amazon.ca%')           OR
+        (domain LIKE '%costco.ca%'           OR event_detail LIKE '%costco.ca%')           OR
+        (domain LIKE '%sobeys.com%'          OR event_detail LIKE '%sobeys.com%')          OR
+        (domain LIKE '%petland.ca%'          OR event_detail LIKE '%petland.ca%')          OR
+        (domain LIKE '%petvalu.ca%'          OR event_detail LIKE '%petvalu.ca%')          OR
+        (domain LIKE '%petsmart.ca%'         OR event_detail LIKE '%petsmart.ca%')         OR
+        (domain LIKE '%baileyblu.com%'       OR event_detail LIKE '%baileyblu.com%')       OR
+        (domain LIKE '%chico.ca%'            OR event_detail LIKE '%chico.ca%')            OR
+        (domain LIKE '%mondou.com%'          OR event_detail LIKE '%mondou.com%')          OR
+        (domain LIKE '%pattesgriffes.com%'   OR event_detail LIKE '%pattesgriffes.com%')   OR
+        (domain LIKE '%tailblazerspets.com%' OR event_detail LIKE '%tailblazerspets.com%') OR
+        (domain LIKE '%wbu.com%'             OR event_detail LIKE '%wbu.com%')                         
+    ) 
+    AND
+    (
+        (event_detail LIKE '%shopping-cart%')                                   OR
+        (event_detail LIKE '%/cart%')                                           OR
+        (event_detail LIKE '%checkout%')                                        OR
+        (event_detail LIKE '%shop%' AND event_detail LIKE '%cart%')             OR
+        (event_detail LIKE '%cart%' AND event_detail LIKE '%shop%')             OR
+        (event_detail LIKE '%order%' AND event_detail LIKE '%history%')         OR
+        (event_detail LIKE '%cart%' AND event_detail LIKE '%checkout%')         OR
+        (event_detail LIKE '%checkout%' AND event_detail LIKE '%cart%')         OR
+        (event_detail LIKE '%recent%' AND event_detail LIKE '%order%')          OR
+        (event_detail LIKE '%order%' AND event_detail LIKE '%recent%')          OR
+        (event_detail LIKE '%account%' AND event_detail LIKE '%order%')         OR
+        (event_detail LIKE '%checkout%' AND event_detail LIKE '%confirm%')      OR
+        (event_detail LIKE '%confirm%' AND event_detail LIKE '%checkout%')
+    )
 )
 
 LIMIT 50000;
