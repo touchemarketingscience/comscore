078a0eec3eba6e940ba82de5232d3f9448ed6896
PY <51064667+pyxn@users.noreply.github.com>
2023-04-05 15:43:28 -0400

    test

--- comscore-canadian-tire/main/main-intenders.sql
+++ comscore-canadian-tire/main/main-intenders.sql
@@ -1,21 +1,26 @@
 SELECT
-    (CASE 
-        WHEN (domain LIKE '%canadiantire.ca%')                  THEN 'Canadian Tire'
-        WHEN (domain LIKE '%walmart.ca%')                       THEN 'Walmart'
-        WHEN (domain LIKE '%amzn%' OR domain LIKE '%amazon%')   THEN 'Amazon'
-        WHEN (domain LIKE '%costco.ca%')                        THEN 'Costco'
-        WHEN (domain LIKE '%sobeys.com%')                       THEN 'Sobeys'
-        WHEN (domain LIKE '%petland.ca%')                       THEN 'Pet Land'
-        WHEN (domain LIKE '%petvalu.ca%')                       THEN 'Pet Valu'
-        WHEN (domain LIKE '%petsmart.ca%')                      THEN 'Pet Smart'
-        WHEN (domain LIKE '%baileyblu.com%')                    THEN 'Bailey Blu'
-        WHEN (domain LIKE '%chico.ca%')                         THEN 'Chico'
-        WHEN (domain LIKE '%mondou.com%')                       THEN 'Mondou'
-        WHEN (domain LIKE '%pattesgriffes.com%')                THEN 'Pattes Griffes'
-        WHEN (domain LIKE '%tailblazerspets.com%')              THEN 'Tail Blazers'
-        WHEN (domain LIKE '%wbu.com%')                          THEN 'Wild Birds Unlimited'
-        ELSE domain
-    END) AS domain_group,
+    (
+        CASE
+            WHEN (domain LIKE '%canadiantire.ca%') THEN 'Canadian Tire'
+            WHEN (domain LIKE '%walmart.ca%') THEN 'Walmart'
+            WHEN (
+                domain LIKE '%amzn%'
+                OR domain LIKE '%amazon%'
+            ) THEN 'Amazon'
+            WHEN (domain LIKE '%costco.ca%') THEN 'Costco'
+            WHEN (domain LIKE '%sobeys.com%') THEN 'Sobeys'
+            WHEN (domain LIKE '%petland.ca%') THEN 'Pet Land'
+            WHEN (domain LIKE '%petvalu.ca%') THEN 'Pet Valu'
+            WHEN (domain LIKE '%petsmart.ca%') THEN 'Pet Smart'
+            WHEN (domain LIKE '%baileyblu.com%') THEN 'Bailey Blu'
+            WHEN (domain LIKE '%chico.ca%') THEN 'Chico'
+            WHEN (domain LIKE '%mondou.com%') THEN 'Mondou'
+            WHEN (domain LIKE '%pattesgriffes.com%') THEN 'Pattes Griffes'
+            WHEN (domain LIKE '%tailblazerspets.com%') THEN 'Tail Blazers'
+            WHEN (domain LIKE '%wbu.com%') THEN 'Wild Birds Unlimited'
+            ELSE domain
+        END
+    ) AS domain_group,
     count (DISTINCT guid)
 FROM
     spectrum_comscore.clickstream_ca
@@ -25,16 +30,36 @@ WHERE
             date_part(year, calendar_date) = 2021
             OR date_part(year, calendar_date) = 2022
         )
+
+        AND (
+                (domain LIKE '%petland.ca%')
+            OR (domain LIKE '%petvalu.ca%')
+            OR (domain LIKE '%petsmart.ca%')
+            OR (domain LIKE '%baileyblu.com%')
+            OR (domain LIKE '%chico.ca%')
+            OR (domain LIKE '%mondou.com%')
+            OR (domain LIKE '%pattesgriffes.com%')
+            OR (domain LIKE '%tailblazerspets.com%')
+            OR (domain LIKE '%wbu.com%')
+        )
         AND (
             (
                 (
-                    domain LIKE '%canadiantire.ca%' AND event_detail LIKE '%pet-care%'      OR
-                    domain LIKE '%canadiantire.ca%' AND event_detail LIKE '%animalerie%' 
+                    domain LIKE '%canadiantire.ca%'
+                    AND event_detail LIKE '%pet-care%'
+                    OR domain LIKE '%canadiantire.ca%'
+                    AND event_detail LIKE '%animalerie%'
                 )
                 OR (domain LIKE '%walmart.ca%')
-                OR (domain LIKE '%amazon%' OR domain LIKE '%amzn%')
+                OR (
+                    domain LIKE '%amazon%'
+                    OR domain LIKE '%amzn%'
+                )
                 OR (domain LIKE '%costco.ca%')
-                OR (domain LIKE '%sobeys.com%' AND event_detail LIKE '%pet%')
+                OR (
+                    domain LIKE '%sobeys.com%'
+                    AND event_detail LIKE '%pet%'
+                )
             )
             AND (
                 event_detail LIKE '%animalerie%'
@@ -51,34 +76,27 @@ WHERE
                 OR event_detail NOT LIKE '%ppet%'
             )
         )
-        OR (domain LIKE '%petland.ca%')
-        OR (domain LIKE '%petvalu.ca%')
-        OR (domain LIKE '%petsmart.ca%')
-        OR (domain LIKE '%baileyblu.com%')
-        OR (domain LIKE '%chico.ca%')
-        OR (domain LIKE '%mondou.com%')
-        OR (domain LIKE '%pattesgriffes.com%')
-        OR (domain LIKE '%tailblazerspets.com%')
-        OR (domain LIKE '%wbu.com%')
+
+        
     )
     /*
-    -- *** CONVERTER *** --
-    AND
-    (
-        -- (event_detail LIKE '%shopping-cart%')                                   OR
-        -- (event_detail LIKE '%/cart%')                                           OR
-        -- (event_detail LIKE '%checkout%')                                        OR
-        -- (event_detail LIKE '%shop%' AND event_detail LIKE '%cart%')             OR
-        -- (event_detail LIKE '%cart%' AND event_detail LIKE '%shop%')             OR
-        
-        (event_detail LIKE '%history%' AND event_detail LIKE '%order%')         OR
-        (event_detail LIKE '%order%' AND event_detail LIKE '%history%')         OR
-        (event_detail LIKE '%recent%' AND event_detail LIKE '%order%')          OR
-        (event_detail LIKE '%order%' AND event_detail LIKE '%recent%')          OR
-        (event_detail LIKE '%account%' AND event_detail LIKE '%order%')         OR 
-        (event_detail LIKE '%order%' AND event_detail LIKE '%account%')         
-    )
-    */
+     -- *** CONVERTER *** --
+     AND
+     (
+     -- (event_detail LIKE '%shopping-cart%')                                   OR
+     -- (event_detail LIKE '%/cart%')                                           OR
+     -- (event_detail LIKE '%checkout%')                                        OR
+     -- (event_detail LIKE '%shop%' AND event_detail LIKE '%cart%')             OR
+     -- (event_detail LIKE '%cart%' AND event_detail LIKE '%shop%')             OR
+     
+     (event_detail LIKE '%history%' AND event_detail LIKE '%order%')         OR
+     (event_detail LIKE '%order%' AND event_detail LIKE '%history%')         OR
+     (event_detail LIKE '%recent%' AND event_detail LIKE '%order%')          OR
+     (event_detail LIKE '%order%' AND event_detail LIKE '%recent%')          OR
+     (event_detail LIKE '%account%' AND event_detail LIKE '%order%')         OR 
+     (event_detail LIKE '%order%' AND event_detail LIKE '%account%')         
+     )
+     */
 GROUP BY
     1
 ORDER BY
