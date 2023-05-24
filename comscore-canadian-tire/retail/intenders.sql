e413e41a99a19455c66fdd92698b5fe65eefc4fc
PY <51064667+pyxn@users.noreply.github.com>
2023-05-24 15:31:02 -0400

    cleanup

--- comscore-canadian-tire/retail/intenders.sql
+++ comscore-canadian-tire/retail/intenders.sql
@@ -28,25 +28,28 @@ comscore_filtered AS (
         END) AS competitor,
         count (DISTINCT guid) as unique_users
     FROM spectrum_comscore.clickstream_ca
-    WHERE (date_part(year, calendar_date) >= (SELECT value FROM year_lower_bound) AND date_part(year, calendar_date) <= (SELECT value FROM year_upper_bound)) AND
-           (domain LIKE '%canadiantire.ca%' OR event_detail LIKE '%canadiantire.ca%')
-        OR (domain LIKE '%amazon.ca%' OR event_detail LIKE '%amazon.ca%')
-        OR (domain LIKE '%walmart.ca%' OR event_detail LIKE '%walmart.ca%')
-        OR (domain LIKE '%bestbuy.ca%' OR event_detail LIKE '%bestbuy.ca%')
-        OR (domain LIKE '%wayfair.ca%' OR event_detail LIKE '%wayfair.ca%')
-        OR (domain LIKE '%ikea.com%' OR event_detail LIKE '%ikea.com%')
-        OR (domain LIKE '%homesense.ca%' OR event_detail LIKE '%homesense.ca%')
-        OR (domain LIKE '%winners.ca%' OR event_detail LIKE '%winners.ca%')
-        OR (domain LIKE '%thebay.com%' OR event_detail LIKE '%thebay.com%')
-        OR (domain LIKE '%labaie.com%' OR event_detail LIKE '%labaie.com%')
-        OR (domain LIKE '%marshalls.ca%' OR event_detail LIKE '%marshalls.ca%')
-        OR (domain LIKE '%homehardware.ca%' OR event_detail LIKE '%homehardware.ca%')
-        OR (domain LIKE '%homedepot.ca%' OR event_detail LIKE '%homedepot.ca%')
-        OR (domain LIKE '%rona.ca%' OR event_detail LIKE '%rona.ca%')
-        OR (domain LIKE '%lowes.ca%' OR event_detail LIKE '%lowes.ca%')
-        OR (domain LIKE '%renodepot.com%' OR event_detail LIKE '%renodepot.com%')
-        OR (domain LIKE '%costco.ca%' OR event_detail LIKE '%costco.ca%')
-        OR (domain LIKE '%dollarama.com%' OR event_detail LIKE '%dollarama.com%')
+    WHERE (date_part(year, calendar_date) >= (SELECT value FROM year_lower_bound) AND date_part(year, calendar_date) <= (SELECT value FROM year_upper_bound)) 
+        AND 
+        (
+               (domain LIKE '%canadiantire.ca%' OR event_detail LIKE '%canadiantire.ca%')
+            OR (domain LIKE '%amazon.ca%' OR event_detail LIKE '%amazon.ca%')
+            OR (domain LIKE '%walmart.ca%' OR event_detail LIKE '%walmart.ca%')
+            OR (domain LIKE '%bestbuy.ca%' OR event_detail LIKE '%bestbuy.ca%')
+            OR (domain LIKE '%wayfair.ca%' OR event_detail LIKE '%wayfair.ca%')
+            OR (domain LIKE '%ikea.com%' OR event_detail LIKE '%ikea.com%')
+            OR (domain LIKE '%homesense.ca%' OR event_detail LIKE '%homesense.ca%')
+            OR (domain LIKE '%winners.ca%' OR event_detail LIKE '%winners.ca%')
+            OR (domain LIKE '%thebay.com%' OR event_detail LIKE '%thebay.com%')
+            OR (domain LIKE '%labaie.com%' OR event_detail LIKE '%labaie.com%')
+            OR (domain LIKE '%marshalls.ca%' OR event_detail LIKE '%marshalls.ca%')
+            OR (domain LIKE '%homehardware.ca%' OR event_detail LIKE '%homehardware.ca%')
+            OR (domain LIKE '%homedepot.ca%' OR event_detail LIKE '%homedepot.ca%')
+            OR (domain LIKE '%rona.ca%' OR event_detail LIKE '%rona.ca%')
+            OR (domain LIKE '%lowes.ca%' OR event_detail LIKE '%lowes.ca%')
+            OR (domain LIKE '%renodepot.com%' OR event_detail LIKE '%renodepot.com%')
+            OR (domain LIKE '%costco.ca%' OR event_detail LIKE '%costco.ca%')
+            OR (domain LIKE '%dollarama.com%' OR event_detail LIKE '%dollarama.com%')
+        )
     GROUP BY 1
     ORDER BY 2 DESC
 )
