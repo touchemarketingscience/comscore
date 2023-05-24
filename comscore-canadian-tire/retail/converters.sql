10bf8fd3c25237e6e4a4122e67259abb528be172
PY <51064667+pyxn@users.noreply.github.com>
2023-05-24 15:25:43 -0400

    add converter and intender definitions

--- comscore-canadian-tire/retail/converters.sql
+++ comscore-canadian-tire/retail/converters.sql
@@ -3,36 +3,62 @@ WITH
 year_lower_bound AS (SELECT 2021 AS value),
 year_upper_bound AS (SELECT 2022 AS value),
 
-comscore_filtered AS (
+comscore_filtered_converters AS (
     SELECT 
         (CASE
         WHEN domain LIKE '%canadiantire.ca%' OR event_detail LIKE '%canadiantire.ca%' THEN 'Canadian Tire'
+        WHEN domain LIKE '%amazon.ca%' OR event_detail LIKE '%amazon.ca%' THEN 'Amazon'
+        WHEN domain LIKE '%walmart.ca%' OR event_detail LIKE '%walmart.ca%' THEN 'Walmart'
+        WHEN domain LIKE '%bestbuy.ca%' OR event_detail LIKE '%bestbuy.ca%' THEN 'Best Buy'
+        WHEN domain LIKE '%wayfair.ca%' OR event_detail LIKE '%wayfair.ca%' THEN 'Wayfair'
+        WHEN domain LIKE '%ikea.com%' OR event_detail LIKE '%ikea.com%' THEN 'IKEA'
+        WHEN domain LIKE '%homesense.ca%' OR event_detail LIKE '%homesense.ca%' THEN 'Home Sense'
+        WHEN domain LIKE '%winners.ca%' OR event_detail LIKE '%winners.ca%' THEN 'Winners'
+        WHEN domain LIKE '%thebay.com%' OR event_detail LIKE '%thebay.com%' THEN 'Hudsons Bay'
+        WHEN domain LIKE '%marshalls.ca%' OR event_detail LIKE '%marshalls.ca%' THEN 'Marshalls'
+        WHEN domain LIKE '%homehardware.ca%' OR event_detail LIKE '%homehardware.ca%' THEN 'Home Hardware'
+        WHEN domain LIKE '%homedepot.ca%' OR event_detail LIKE '%homedepot.ca%' THEN 'Home Depot'
+        WHEN domain LIKE '%rona.ca%' OR event_detail LIKE '%rona.ca%' THEN 'Rona'
+        WHEN domain LIKE '%lowes.ca%' OR event_detail LIKE '%lowes.ca%' THEN 'Lowes'
+        WHEN domain LIKE '%renodepot.com%' OR event_detail LIKE '%renodepot.com%' THEN 'Reno Depot'
+        WHEN domain LIKE '%costco.ca%' OR event_detail LIKE '%costco.ca%' THEN 'Costco'
+        WHEN domain LIKE '%dollarama.com%' OR event_detail LIKE '%dollarama.com%' THEN 'Dollarama'
         ELSE domain
         END) AS competitor,
-        event_detail,
-        event_detail2,
         count (DISTINCT guid)
     FROM spectrum_comscore.clickstream_ca
-    WHERE (date_part(year, calendar_date) >= (SELECT value FROM year_lower_bound) AND date_part(year, calendar_date) <= (SELECT value FROM year_upper_bound)) AND
-        (domain LIKE '%canadiantire.ca%' OR event_detail LIKE '%canadiantire.ca%') AND (
-        (event_detail2 LIKE '%convert%')                                        OR
-        (event_detail2 LIKE '%checkout%')                                       OR
-        (event_detail LIKE '%/commande%')                                       OR
-        (event_detail LIKE '%shopping-cart%')                                   OR
-        (event_detail LIKE '%/cart%')                                           OR
-        (event_detail LIKE '%checkout%')                                        OR
-        (event_detail LIKE '%shop%' AND event_detail LIKE '%cart%')             OR
-        (event_detail LIKE '%cart%' AND event_detail LIKE '%shop%')             OR
-        (event_detail LIKE '%history%' AND event_detail LIKE '%order%')         OR
-        (event_detail LIKE '%order%' AND event_detail LIKE '%history%')         OR
-        (event_detail LIKE '%recent%' AND event_detail LIKE '%order%')          OR
-        (event_detail LIKE '%order%' AND event_detail LIKE '%recent%')          OR
-        (event_detail LIKE '%account%' AND event_detail LIKE '%order%')         OR 
-        (event_detail LIKE '%order%' AND event_detail LIKE '%account%')         
-    )
-    GROUP BY 1, 2, 3
-    ORDER BY 4 DESC
+    WHERE (date_part(year, calendar_date) >= (SELECT value FROM year_lower_bound) AND date_part(year, calendar_date) <= (SELECT value FROM year_upper_bound)) 
+        AND
+        ( -- INTENDER LOGIC
+                (domain LIKE '%canadiantire.ca%' OR event_detail LIKE '%canadiantire.ca%')
+            OR (domain LIKE '%amazon.ca%' OR event_detail LIKE '%amazon.ca%')
+            OR (domain LIKE '%walmart.ca%' OR event_detail LIKE '%walmart.ca%')
+            OR (domain LIKE '%bestbuy.ca%' OR event_detail LIKE '%bestbuy.ca%')
+            OR (domain LIKE '%wayfair.ca%' OR event_detail LIKE '%wayfair.ca%')
+            OR (domain LIKE '%ikea.com%' OR event_detail LIKE '%ikea.com%')
+            OR (domain LIKE '%homesense.ca%' OR event_detail LIKE '%homesense.ca%')
+            OR (domain LIKE '%winners.ca%' OR event_detail LIKE '%winners.ca%')
+            OR (domain LIKE '%thebay.com%' OR event_detail LIKE '%thebay.com%')
+            OR (domain LIKE '%marshalls.ca%' OR event_detail LIKE '%marshalls.ca%')
+            OR (domain LIKE '%homehardware.ca%' OR event_detail LIKE '%homehardware.ca%')
+            OR (domain LIKE '%homedepot.ca%' OR event_detail LIKE '%homedepot.ca%')
+            OR (domain LIKE '%rona.ca%' OR event_detail LIKE '%rona.ca%')
+            OR (domain LIKE '%lowes.ca%' OR event_detail LIKE '%lowes.ca%')
+            OR (domain LIKE '%renodepot.com%' OR event_detail LIKE '%renodepot.com%')
+            OR (domain LIKE '%costco.ca%' OR event_detail LIKE '%costco.ca%')
+            OR (domain LIKE '%dollarama.com%' OR event_detail LIKE '%dollarama.com%')
+        ) 
+        AND
+        (  -- CONVERTER LOGIC
+                event_detail LIKE '%checkout%' 
+            OR event_detail LIKE '%commande%'
+            OR event_detail LIKE '%payment%'
+            OR event_detail LIKE '%storelocator%'
+            OR event_detail LIKE '%caisse%'
+        )
+    GROUP BY 1
+    ORDER BY 2 DESC
 )
 
-SELECT * FROM comscore_filtered
+SELECT * FROM comscore_filtered_converters 
 LIMIT 10000;
