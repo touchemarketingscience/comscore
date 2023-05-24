f93eb95516bb905f338cbe8d8ebc56ff9f695b92
PY <51064667+pyxn@users.noreply.github.com>
2023-05-24 15:30:08 -0400

    add the bay french

--- comscore-canadian-tire/retail/converters.sql
+++ comscore-canadian-tire/retail/converters.sql
@@ -15,6 +15,7 @@ comscore_filtered_converters AS (
         WHEN domain LIKE '%homesense.ca%' OR event_detail LIKE '%homesense.ca%' THEN 'Home Sense'
         WHEN domain LIKE '%winners.ca%' OR event_detail LIKE '%winners.ca%' THEN 'Winners'
         WHEN domain LIKE '%thebay.com%' OR event_detail LIKE '%thebay.com%' THEN 'Hudsons Bay'
+        WHEN domain LIKE '%labaie.com%' OR event_detail LIKE '%labaie.com%' THEN 'Hudsons Bay'
         WHEN domain LIKE '%marshalls.ca%' OR event_detail LIKE '%marshalls.ca%' THEN 'Marshalls'
         WHEN domain LIKE '%homehardware.ca%' OR event_detail LIKE '%homehardware.ca%' THEN 'Home Hardware'
         WHEN domain LIKE '%homedepot.ca%' OR event_detail LIKE '%homedepot.ca%' THEN 'Home Depot'
@@ -30,7 +31,7 @@ comscore_filtered_converters AS (
     WHERE (date_part(year, calendar_date) >= (SELECT value FROM year_lower_bound) AND date_part(year, calendar_date) <= (SELECT value FROM year_upper_bound)) 
         AND
         ( -- INTENDER LOGIC
-                (domain LIKE '%canadiantire.ca%' OR event_detail LIKE '%canadiantire.ca%')
+               (domain LIKE '%canadiantire.ca%' OR event_detail LIKE '%canadiantire.ca%')
             OR (domain LIKE '%amazon.ca%' OR event_detail LIKE '%amazon.ca%')
             OR (domain LIKE '%walmart.ca%' OR event_detail LIKE '%walmart.ca%')
             OR (domain LIKE '%bestbuy.ca%' OR event_detail LIKE '%bestbuy.ca%')
@@ -39,6 +40,7 @@ comscore_filtered_converters AS (
             OR (domain LIKE '%homesense.ca%' OR event_detail LIKE '%homesense.ca%')
             OR (domain LIKE '%winners.ca%' OR event_detail LIKE '%winners.ca%')
             OR (domain LIKE '%thebay.com%' OR event_detail LIKE '%thebay.com%')
+            OR (domain LIKE '%labaie.com%' OR event_detail LIKE '%labaie.com%')
             OR (domain LIKE '%marshalls.ca%' OR event_detail LIKE '%marshalls.ca%')
             OR (domain LIKE '%homehardware.ca%' OR event_detail LIKE '%homehardware.ca%')
             OR (domain LIKE '%homedepot.ca%' OR event_detail LIKE '%homedepot.ca%')
