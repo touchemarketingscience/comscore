a716dcc60a897c8970f117f76c07846f5f73d2a9
PY <51064667+pyxn@users.noreply.github.com>
2023-06-01 13:55:35 -0400

    update category codes

--- comscore-canadian-tire/retail/05-journey/time-spent-by-domain.sql
+++ comscore-canadian-tire/retail/05-journey/time-spent-by-domain.sql
@@ -6,9 +6,10 @@ date_upper_bound AS (SELECT '2022-12-31' AS value),
 unique_intender_data AS (
     SELECT 
     calendar_date,
+    event_time,
     event_detail,
+    time_spent,
     domain,
-    zvelo_subcategory,
     guid,
     (CASE
     WHEN domain LIKE '%canadiantire.ca%' OR event_detail LIKE '%canadiantire.ca%' THEN 'Canadian Tire'
@@ -58,32 +59,55 @@ unique_intender_data AS (
 unique_converter_data AS (
     SELECT 
     calendar_date,
+    event_time,
     event_detail,
+    time_spent,
     domain,
-    zvelo_subcategory,
     guid,
     domain_group
     FROM unique_intender_data
-    WHERE 
-    (  -- CONVERTER LOGIC
-           event_detail LIKE '%checkout%' 
-        OR event_detail LIKE '%commande%'
-        OR event_detail LIKE '%payment%'
-        OR event_detail LIKE '%caisse%'
-    )
+    WHERE ((calendar_date) >= (SELECT value FROM date_lower_bound) AND calendar_date <= (SELECT value FROM date_upper_bound))
+    AND
+        (  -- CONVERTER LOGIC
+               event_detail LIKE '%checkout%' 
+            OR event_detail LIKE '%commande%'
+            OR event_detail LIKE '%payment%'
+            OR event_detail LIKE '%caisse%'
+        )
 ),
 
 unique_locator_data AS (
     SELECT 
     calendar_date,
+    event_time,
     event_detail,
+    time_spent,
     domain,
-    zvelo_subcategory,
     guid,
-    domain_group
-    FROM unique_intender_data
-    WHERE
-    ( -- LOCATOR LOGIC
+    (CASE
+    WHEN domain LIKE '%canadiantire.ca%' OR event_detail LIKE '%canadiantire.ca%' THEN 'Canadian Tire'
+    WHEN domain LIKE '%amazon.ca%' OR event_detail LIKE '%amazon.ca%' THEN 'Amazon'
+    WHEN domain LIKE '%walmart.ca%' OR event_detail LIKE '%walmart.ca%' THEN 'Walmart'
+    WHEN domain LIKE '%bestbuy.ca%' OR event_detail LIKE '%bestbuy.ca%' THEN 'Best Buy'
+    WHEN domain LIKE '%wayfair.ca%' OR event_detail LIKE '%wayfair.ca%' THEN 'Wayfair'
+    WHEN domain LIKE '%ikea.com%' OR event_detail LIKE '%ikea.com%' THEN 'IKEA'
+    WHEN domain LIKE '%homesense.ca%' OR event_detail LIKE '%homesense.ca%' THEN 'Home Sense'
+    WHEN domain LIKE '%winners.ca%' OR event_detail LIKE '%winners.ca%' THEN 'Winners'
+    WHEN domain LIKE '%thebay.com%' OR event_detail LIKE '%thebay.com%' THEN 'Hudsons Bay'
+    WHEN domain LIKE '%labaie.com%' OR event_detail LIKE '%labaie.com%' THEN 'Hudsons Bay'
+    WHEN domain LIKE '%marshalls.ca%' OR event_detail LIKE '%marshalls.ca%' THEN 'Marshalls'
+    WHEN domain LIKE '%homehardware.ca%' OR event_detail LIKE '%homehardware.ca%' THEN 'Home Hardware'
+    WHEN domain LIKE '%homedepot.ca%' OR event_detail LIKE '%homedepot.ca%' THEN 'Home Depot'
+    WHEN domain LIKE '%rona.ca%' OR event_detail LIKE '%rona.ca%' THEN 'Rona'
+    WHEN domain LIKE '%lowes.ca%' OR event_detail LIKE '%lowes.ca%' THEN 'Lowes'
+    WHEN domain LIKE '%renodepot.com%' OR event_detail LIKE '%renodepot.com%' THEN 'Reno Depot'
+    WHEN domain LIKE '%costco.ca%' OR event_detail LIKE '%costco.ca%' THEN 'Costco'
+    WHEN domain LIKE '%dollarama.com%' OR event_detail LIKE '%dollarama.com%' THEN 'Dollarama'
+    ELSE domain
+    END) AS domain_group
+    FROM spectrum_comscore.clickstream_ca
+    WHERE ((calendar_date) >= (SELECT value FROM date_lower_bound) AND calendar_date <= (SELECT value FROM date_upper_bound))
+    AND ( -- LOCATOR LOGIC
             ((domain LIKE '%canadiantire.ca%' OR event_detail LIKE '%canadiantire.ca%') AND (event_detail LIKE '%store-locator%' OR event_detail LIKE '%localisateur-de-magasin%'))
             OR ((domain LIKE '%amazon.ca%'  OR event_detail LIKE '%amazon.ca%') AND (event_detail LIKE '%store-locator%' OR event_detail LIKE '%localisateur-de-magasin%'))
             OR ((domain LIKE '%walmart.ca%' OR event_detail LIKE '%walmart.ca%') AND (event_detail LIKE '%stores-near-me%' OR event_detail LIKE  '%magasin%'))
@@ -108,7 +132,6 @@ unique_locator_data AS (
 -- *********************************************************************************************
 --  GUID LIST
 -- *********************************************************************************************
-
 intender_list AS (
     SELECT DISTINCT guid FROM unique_intender_data
 ),
@@ -120,71 +143,6 @@ converter_list AS (
 locator_list AS (
     SELECT DISTINCT guid FROM unique_locator_data
 ),
-
--- *********************************************************************************************
---  TOTAL INTENDERS, CONVERTERS, LOCATORS, GENPOP
--- *********************************************************************************************
-
-total_intenders AS (
-    SELECT
-        UPPER(REPLACE(REPLACE(zvelo_subcategory, ' and ', '&'),' ','')) AS join_field_a,
-        COUNT(DISTINCT guid) AS unique_users
-    FROM spectrum_comscore.clickstream_ca
-    WHERE ((calendar_date) >= (SELECT value FROM date_lower_bound) AND calendar_date <= (SELECT value FROM date_upper_bound)) AND guid IN (
-        SELECT guid FROM intender_list
-    )
-    GROUP BY 1
-),
-
-total_converters AS (
-    SELECT
-        UPPER(REPLACE(REPLACE(zvelo_subcategory, ' and ', '&'),' ','')) AS join_field_a,
-        COUNT(DISTINCT guid) AS unique_users
-    FROM spectrum_comscore.clickstream_ca
-    WHERE ((calendar_date) >= (SELECT value FROM date_lower_bound) AND calendar_date <= (SELECT value FROM date_upper_bound)) AND guid IN (
-        SELECT guid FROM converter_list
-    )
-    GROUP BY 1
-),
-
-total_locators AS (
-    SELECT
-        UPPER(REPLACE(REPLACE(zvelo_subcategory, ' and ', '&'),' ','')) AS join_field_a,
-        COUNT(DISTINCT guid) AS unique_users
-    FROM spectrum_comscore.clickstream_ca
-    WHERE ((calendar_date) >= (SELECT value FROM date_lower_bound) AND calendar_date <= (SELECT value FROM date_upper_bound)) AND guid IN (
-        SELECT guid FROM locator_list
-    )
-    GROUP BY 1
-),
-
-total_genpop AS (
-     SELECT
-        UPPER(REPLACE(REPLACE(zvelo_subcategory, ' and ', '&'),' ','')) AS join_field_a,
-        COUNT(DISTINCT guid) AS unique_users
-    FROM spectrum_comscore.clickstream_ca 
-    WHERE ((calendar_date) >= (SELECT value FROM date_lower_bound) AND calendar_date <= (SELECT value FROM date_upper_bound)) 
-    GROUP BY 1
-),
-
-
--- *********************************************************************************************
---  MAIN OUTPUT DATA
--- *********************************************************************************************
-
-total_output AS (
-    SELECT 
-        total_intenders.join_field_a    AS join_field_a,
-        total_intenders.unique_users    AS total_intenders,
-        total_converters.unique_users   AS total_converters,
-        total_locators.unique_users     AS total_locators,
-        total_genpop.unique_users       AS total_genpop
-    FROM total_intenders
-    LEFT JOIN total_genpop      ON total_intenders.join_field_a = total_genpop.join_field_a
-    LEFT JOIN total_converters  ON total_intenders.join_field_a = total_converters.join_field_a 
-    LEFT JOIN total_locators    ON total_intenders.join_field_a = total_locators.join_field_a 
-),
-
 -- *********************************************************************************************
 --  INDEX REFERENCE COLUMNS
 -- *********************************************************************************************
@@ -208,52 +166,22 @@ ref_converters AS (
 ref_locators AS (
     SELECT COUNT(DISTINCT guid) AS unique_users
     FROM locator_list
+),
+
+timespent_per_user_per_domain AS (
+SELECT 
+    unique_intender_data.domain_group AS competitor,
+    unique_intender_data.guid AS guid,
+    AVG(COALESCE(unique_intender_data.time_spent, 0)) AS avg_time_spent
+FROM unique_intender_data
+GROUP BY competitor, guid
 )
 
--- *********************************************************************************************
---  OUTPUT
--- *********************************************************************************************
-
 SELECT
-    total_output.join_field_a           AS zvelo_subcategory,
-    
-    total_output.total_genpop           AS total_genpop,
-    total_output.total_intenders        AS total_intenders,
-    total_output.total_converters       AS total_converters,
-    total_output.total_locators         AS total_locators,
+    competitor,
+    AVG(avg_time_spent) AS avg_time_spent_per_user
+FROM timespent_per_user_per_domain
+GROUP BY competitor
 
-    ref_genpop.unique_users             AS ref_genpop,
-    ref_intenders.unique_users          AS ref_intenders,
-    ref_converters.unique_users         AS ref_converters,
-    ref_locators.unique_users           AS ref_locators
-    
-FROM        total_output
-CROSS JOIN  ref_genpop
-CROSS JOIN  ref_intenders
-CROSS JOIN  ref_converters
-CROSS JOIN  ref_locators
 
--- COMPETITIVE SET EXCLUSION+VOLUME FILTER
-WHERE total_intenders > 50 AND NOT 
-( -- INTENDER LOGIC NEGATION
-            (domain LIKE '%canadiantire.ca%')
-        OR (domain LIKE '%amazon.ca%')
-        OR (domain LIKE '%walmart.ca%')
-        OR (domain LIKE '%bestbuy.ca%')
-        OR (domain LIKE '%wayfair.ca%')
-        OR (domain LIKE '%ikea.com%' )
-        OR (domain LIKE '%homesense.ca%')
-        OR (domain LIKE '%winners.ca%')
-        OR (domain LIKE '%thebay.com%')
-        OR (domain LIKE '%labaie.com%')
-        OR (domain LIKE '%marshalls.ca%')
-        OR (domain LIKE '%homehardware.ca%')
-        OR (domain LIKE '%homedepot.ca%')
-        OR (domain LIKE '%rona.ca%')
-        OR (domain LIKE '%lowes.ca%')
-        OR (domain LIKE '%renodepot.com%')
-        OR (domain LIKE '%costco.ca%')
-        OR (domain LIKE '%dollarama.com%')
-   )
-ORDER BY 2 DESC
-LIMIT 100000;
+LIMIT 500000;
